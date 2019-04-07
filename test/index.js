79c0eba379995e8f8cde3d946a5897d809a6fe2f
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-07 18:45:42 +0200

    example of usage

---
+++ test/index.js
@@ -0,0 +1,19 @@
+const ddbl_api = require('../');
+const ddbl = new ddbl_api('BOT_ID', 'API_KEY');
+
+
+/* Example postStats usage */
+ddbl.postStats('BOT_ID')
+    .then(console.log);
+
+/* Example getStats usage */
+ddbl.getStats('BOT_ID')
+    .then(console.log);
+
+/* Example postStats usage */
+ddbl.getVotes()
+    .then(console.log);
+
+/* Example hasVoted24 usage */
+ddbl.hasVoted24('USER_ID')
+    .then(console.log);
